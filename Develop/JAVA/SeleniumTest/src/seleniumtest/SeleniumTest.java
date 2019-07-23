ec23531c44ae31dedc7d999b2670f01213cf0d65
Sasha Harp <sashaharp@gmail.com>
2019-07-23 14:10:24 +0200

    selenium fail

---
+++ Develop/JAVA/SeleniumTest/src/seleniumtest/SeleniumTest.java
@@ -0,0 +1,20 @@
+package seleniumtest;
+
+import java.io.File;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+/**
+ * @author sashaharp
+ */
+
+public class SeleniumTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
